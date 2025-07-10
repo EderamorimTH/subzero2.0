8b401e8c8906e09ea703d2867b9fa60e1953dd82
EderamorimTH <eder.thiago123@gmail.com>
2025-07-09 23:22:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://larrisasubzero.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://ederamorimth.github.io');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'OPTIONS') {
